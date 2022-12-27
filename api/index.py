504c4dffd998ba4e9ecaa60a6e02902294c096c0
Brent Nequin <70235148+brentnequin@users.noreply.github.com>
2022-12-27 00:59:41 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         self.wfile.write('Hello, world!'.encode('utf-8'))
+#         return
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def index():
+    if(request.method == 'GET'):
+        return jsonify({
+            status: 200
+            message: 'Hello World'
+        })
