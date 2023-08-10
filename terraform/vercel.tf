725c4f8909662bbcdc6e81f96d516aaaf7b4c10a
Brent Nequin <brentnequin@gmail.com>
2023-08-09 23:30:46 -0500

    maybe

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -7,6 +7,18 @@ resource "vercel_project" "frontend" {
         repo = "brentnequin/brentnequin"
     }
 
-    build_command = "cd .. && npx turbo run build --scope=frontend"
-    root_directory = "frontend"
+    # build_command = "cd .. && npx turbo run build --scope=frontend"
+    build_command = "npx turbo run build --scope=frontend"
+    # root_directory = "frontend"
+}
+
+resource "vercel_project_domain" "frontend" {
+    project_id = vercel_project.frontend.id
+    domain     = "brentnequin.com"
+}
+
+resource "vercel_deployment" "frontend" {
+    project_id  = vercel_project.frontend.id
+    ref        = "main"
+    production  = true
 }
