a3905956b31325079692efa7d9f98fe7ec6ab068
Brent Nequin <brentnequin@gmail.com>
2026-06-22 19:44:13 -0500

    app footer, header

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
+
+<template>
+  <footer class="py-8">
+    <p class="text-center text-sm text-zinc-500">
+      © {{ year }} Brent Nequin
+    </p>
+  </footer>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex min-h-screen flex-col bg-gray-950 text-white">
+    <AppHeader />
+
+    <main class="flex-1">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
