ceb06d1a0b59974dbd6fcd10e5c10977566e3a37
Brent Nequin <brentnequin@gmail.com>
2026-06-25 00:24:12 -0500

    remove redundant spacing

--- pages/projects.vue
+++ pages/projects.vue
@@ -47,7 +47,7 @@ const projects = ref([
 </script>
 
 <template>
-  <div class="mx-auto max-w-5xl px-6 py-16">
+  <div>
     <div class="mb-12">
       <h1 class="text-4xl font-bold">Projects</h1>
       <p class="mt-3 text-neutral-400">
